500780f1abb850e1895ed9bbde28129262ac5609
KenleyundLeon <muthkenley6@gmail.com>
2024-05-12 17:50:36 +0200

    new file:   api/installation.js 	deleted:    api/loadwebserver.js 	modified:   package-lock.json 	modified:   package.json 	modified:   resources/scripts/loadAPIS.js 	new file:   resources/storage/favicon.ico 	renamed:    resources/views/installation/index.html -> resources/views/installation/index.php

---
+++ api/installation.js
@@ -0,0 +1,11 @@
+const fs = require('node:fs');
+const path = require('node:path');
+
+module.exports = {
+    name: "installation",
+    method: "PHP",
+    path: "/",
+    callback: (req, res, dir) => {
+        res.status(200).render(path.join(dir, "resources", "views", "installation", "index.php"));
+    }
+};
--- resources/scripts/loadAPIS.js
+++ resources/scripts/loadAPIS.js
@@ -1,12 +1,12 @@
-const fs = require("node:fs");
-const path = require("node:path");
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
 const bodyParser = require('body-parser');
+const phpExpress = require('php-express')({binPath: 'php'});
 
-module.exports = 
-function loadAPIS(app, dir){
+module.exports = function loadAPIS(app, dir) {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
-
     app.use((req, res, next) => {
         res.setHeader('Access-Control-Allow-Origin', '*');
         res.header('Access-Control-Allow-Methods', 'GET, POST');
@@ -14,18 +14,25 @@ function loadAPIS(app, dir){
         next();
     });
 
-    let files = fs.readdirSync(path.join(dir, "/api"), (err, data) => {if(err){return console.log(err);};return data;});
+    let files = fs.readdirSync(path.join(dir, "/api"));
     for (let i = 0; i < files.length; i++) {
         let mod = require(path.join(dir, "/api", files[i]));
         try {
-            if(!mod.name){mod.name = "undefined"}else{mod.name = mod.name};
-            if(!mod.method || !mod.path){console.warn(`Error: ${mod.name}`);return;};
-            app[mod.method.toLowerCase()](mod.path, (req, res, __dirname) => {
+            if(mod.method === "PHP"){
+                app.engine('php', phpExpress.engine);
+                app.all(/.+\.php$/, phpExpress.router);
+                app.set('view engine', 'php');
+                app.use(mod.path, (req, res) => {
+                    mod.callback(req, res, dir);
+                });
+            }else{
+            app[mod.method.toLowerCase()](mod.path, (req, res) => {
                 mod.callback(req, res, dir);
             });
+            }
             console.log(`Loaded: ${mod.name} | ${mod.method} | ${mod.path}`);
         } catch (err) {
             console.log(err);
         }
     }
-}
+};
