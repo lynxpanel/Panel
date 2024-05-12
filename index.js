8e6c06247779f9aa63efb8f29324e05069b20eed
KenleyundLeon <muthkenley6@gmail.com>
2024-05-12 03:14:44 +0200

    modified:   index.js 	new file:   resources/scripts/loadAPIS.js

--- index.js
+++ index.js
@@ -1,20 +1,9 @@
 require('dotenv').config();
-const fs = require("node:fs");
-const path = require("node:path");
 const express = require("express");
 const bodyParser = require('body-parser');
+const loadAPIS = require('./resources/scripts/loadAPIS');
 
 const app = express();
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
 
 if(process.env.DB_TYPE === "sqlite"){
 
@@ -26,29 +15,11 @@ if(process.env.DB_TYPE === "sqlite"){
     return console.warn("Wrong Database type specified.");
 }
 
-
-function loadAPIS(app){
-    let files = fs.readdirSync(path.join(__dirname, "/api"), (err, data) => {if(err){return console.log(err);};return data;});
-    for (let i = 0; i < files.length; i++) {
-        let mod = require(path.join(__dirname, "/api", files[i]));
-        try {
-            if(!mod.name){mod.name = "undefined"}else{mod.name = mod.name};
-            if(!mod.method || !mod.path){console.warn(`Error: ${mod.name}`);return;};
-            app[mod.method.toLowerCase()](mod.path, (req, res) => {
-                mod.callback(req, res);
-            });
-            console.log(`Loaded: ${mod.name} | ${mod.method} | ${mod.path}`);
-        } catch (err) {
-            console.log(err);
-        }
-    }
-}
-
 try {
     console.clear();
-    app.listen(process.env.APP_PORT, () => {
+    app.listen(process.env.APP_PORT, process.env.APP_IP, () => {
         console.log(`Panel running on: ${process.env.APP_IP}:${process.env.APP_PORT}`);
-        loadAPIS(app);
+        loadAPIS(app, __dirname);
     });
 } catch (error) {
 	console.warn(error);
---
+++ resources/scripts/loadAPIS.js
@@ -0,0 +1,32 @@
+const fs = require("node:fs");
+const path = require("node:path");
+const bodyParser = require('body-parser');
+
+module.exports = 
+
+function loadAPIS(app, dir){
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    app.use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET, POST');
+        res.header('Access-Control-Allow-Headers', 'Content-Type');
+        next();
+    });
+
+    let files = fs.readdirSync(path.join(dir, "/api"), (err, data) => {if(err){return console.log(err);};return data;});
+    for (let i = 0; i < files.length; i++) {
+        let mod = require(path.join(dir, "/api", files[i]));
+        try {
+            if(!mod.name){mod.name = "undefined"}else{mod.name = mod.name};
+            if(!mod.method || !mod.path){console.warn(`Error: ${mod.name}`);return;};
+            app[mod.method.toLowerCase()](mod.path, (req, res) => {
+                mod.callback(req, res);
+            });
+            console.log(`Loaded: ${mod.name} | ${mod.method} | ${mod.path}`);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+}
