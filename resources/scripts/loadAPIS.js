3ccfd207b7422ab6dca625ef6537c0168e6a0464
KenleyundLeon <muthkenley6@gmail.com>
2024-05-12 19:50:44 +0200

    modified:   resources/scripts/loadAPIS.js

--- resources/scripts/loadAPIS.js
+++ resources/scripts/loadAPIS.js
@@ -9,7 +9,7 @@ module.exports = function loadAPIS(app, dir) {
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use((req, res, next) => {
         res.setHeader('Access-Control-Allow-Origin', '*');
-        res.header('Access-Control-Allow-Methods', 'GET, POST');
+        res.header('Access-Control-Allow-Methods', '*');
         res.header('Access-Control-Allow-Headers', 'Content-Type');
         next();
     });
